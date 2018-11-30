bce47d20c8eca0c780a99af59340f26b5d415308
duofuni <qazwsx>
2018-12-01 01:24:05 +0800

    m

--- server/src/utils/imgUpload.js
+++ server/src/utils/imgUpload.js
@@ -3,7 +3,7 @@ const multer = require('multer');
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-      cb(null, path.join(__dirname, '../../' , '/images'));
+      cb(null, path.join(__dirname, '../../' , 'images'));
     },
     filename: (req, file, cb) => {
       cb(null, file.originalname);
