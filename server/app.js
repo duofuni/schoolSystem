db4a076016791eaeb149ea97c2c94100f750c948
duofuni <qazwsx>
2018-12-01 01:08:39 +0800

    m

--- server/app.js
+++ server/app.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.use(express.static('./public')); // 使用静态资源
+app.use(express.static('./')); // 使用静态资源
 
 const settings = require('./config/settings');
 const router = require('./src/router/router');
